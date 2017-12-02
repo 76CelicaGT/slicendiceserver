6340e070bdfaa1bc87d6352960e12adaaedf1a11
Austin Jones <Bbace21@gmail.com>
2017-12-02 01:25:34 -0700

    server

--- index.js
+++ index.js
@@ -2,11 +2,43 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 const app = express()
+let changes = []
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.get('/', (req, res) => res.render('pages/index'))
+//app.get('/test', (req, res) => res.send('Hello world'))
 
-  app.use(express.static(path.join(__dirname, 'public')))
-  app.set('views', path.join(__dirname, 'views'))
-  app.set('view engine', 'ejs')
-  app.get('/', (req, res) => res.render('pages/index'))
-  app.get('/test', (req, res) => res.send('Hello world'))
+//gets turn number and most recent change
+app.get('/poll', (req, res) => {
+    res.send({
+     changes: changes[changes.length-1],
+     turnNumber: changes.length,
+    })
+})
+
+//updates map with most recent changes
+app.post('/makeChanges', (req, res) => {
+ changes.push(req.body.changes)
+ res.send(200)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
