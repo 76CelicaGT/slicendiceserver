8e4c62642d6a987e019ec92b90ff92bfac450b47
Austin Jones <Bbace21@gmail.com>
2017-12-01 23:10:11 -0700

    new

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const app = express()
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/test', (req, res) => res.send('Hello world'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  app.use(express.static(path.join(__dirname, 'public')))
+  app.set('views', path.join(__dirname, 'views'))
+  app.set('view engine', 'ejs')
+  app.get('/', (req, res) => res.render('pages/index'))
+  app.get('/test', (req, res) => res.send('Hello world'))
+  app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
